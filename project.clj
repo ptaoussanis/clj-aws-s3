d5214120c200e6757f2ad72a8657e320ad57ea96
Peter Taoussanis <peter@taoensso.com>
2025-08-06 12:43:14 +0200

    [new] Update S3 dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.7.5"]
-                 [clj-time "0.6.0"]]
-  :plugins [[codox "0.8.10"]])
+                 [com.amazonaws/aws-java-sdk-s3 "1.12.788"]
+                 [clj-time "0.6.0"]])
